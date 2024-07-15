fe60555877894bcfe46b70a6b27a9826e8b52c2a
GaganKalra <135148767+GaganKalra@users.noreply.github.com>
2024-07-15 23:53:49 +0530

    Add files via upload

---
+++ basic1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+ int main(){
+ /* int a , b ,res;
+  a = 10;
+  b =20;
+
+  res = a+b;
+
+  printf("%d \n",a);
+  printf("%d %d \n", a , b);  
+  printf("%d %d %d  \n", a , b ,res);
+printf("Addition  of %d and %d = %d \n", a, b , res);
+  printf(" (%d) + (%d) = (%d)", a , b ,res); */
+
+/*int a , b ,res;
+scanf("%d",&a);
+scanf("%d",&b);
+
+res  =a+b;
+printf("Addition  of %d and %d = %d \n", a, b , res); */
+ int a , b ,res;
+ printf("Enter Two numbers :");
+scanf("%d %d",&a , &b);
+
+
+res  =a+b;
+printf("Addition  of %d and %d = %d \n", a, b , res);
+  return 0;  
+ }
